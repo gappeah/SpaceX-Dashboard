3b11d2ff19be3f59ab680d7c8285dc533e4b09cd
George <appeahg@outlook.com>
2023-12-23 14:49:25 +0000

    Defined categories

--- app.py
+++ app.py
@@ -8,16 +8,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def fetch_spacex_lauches():
+def fetch_spacex_launches():
     url = 'https://api.spacexdata.com/v4/launches'
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
-        return[]
+        return[]  
     
-launches = fetch_spacex_lauches()
-print(launches[0])
+def category(Launches):
+    success = list(filter(Launches, lambda x: x['launch success'] and not x ["upcoming"]))
+    failed = list(filter(Launches, lambda x: not x['launch failed'] and not x ["upcoming"]))
+    upcoming = list(filter(Launches, lambda x: x ["upcoming"]))
+    
+
+    
+launches = fetch_spacex_launches()
+
+
+
 
 
 
